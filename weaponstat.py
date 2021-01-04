96771e418af6ef070d1ed51df57cbc4621cf8a29
Diogenis <dio.tsolakou@hotmail.com>
2021-01-04 02:10:03 +0200

    v1.01 Fixes to how the data looks

--- weaponstat.py
+++ weaponstat.py
@@ -1,10 +1,8 @@
 import discord
 import csv
 
-token = open("token.txt", "r").read()
+token = open('token.txt', 'r').read()
 client = discord.Client()
-file = open("R6S Weapon Stats.csv")
-weapon_data = csv.reader(file, delimiter=',')
 weapon_list = []
 
 
@@ -36,7 +34,14 @@ class Weapon:
         return self.ads
 
 
+def tuple_to_string(tuple_to_change):
+    string = ''.join(tuple_to_change)
+    return string
+
+
 def read_file(weapon_name):
+    file = open('R6S Weapon Stats.csv')
+    weapon_data = csv.reader(file, delimiter=',')
     weapon_to_find = weapon_name
     weapon_to_find = weapon_to_find.upper()
     found = False
@@ -47,26 +52,48 @@ def read_file(weapon_name):
         found = True
 
     if not found:
-        print("Please provide proper weapon name. Including hyphens")
+        print('Please provide proper weapon name. Including hyphens')
 
     file.seek(0)
 
-    header = ("Weapon\t Damage\t DPS\t STK vs 1 armor\t TTK vs 1 armor\t STK vs 2 armor\t TTK vs 2 armor\t "
-          "STK vs 3 armor/1+rook\t TTK vs 3 armor/1+rook\t Rate of Fire\n")
+    #For debugging
+#    print('Weapon\t Damage\t DPS\t STK vs 1 armor\t TTK vs 1 armor\t STK vs 2 armor\t TTK vs 2 armor\t '
+#          'STK vs 3 armor/1+rook\t TTK vs 3 armor/1+rook\t Rate of Fire\n')
+    #For debugging
+#    print('{:>3}  {:>6}  {:>5} {:>12}  {:>14}  {:>14} {:>14}  {:>18}  {:>22} {:>20} '.format(
+#        weapon_list[0].get_name(), weapon_list[0].get_stat(), weapon_list[1].get_stat(),
+#        weapon_list[2].get_stat(), weapon_list[3].get_stat(), weapon_list[4].get_stat(),
+#        weapon_list[5].get_stat(), weapon_list[6].get_stat(), weapon_list[7].get_stat(), weapon_list[0].get_rof()))
 
-    stats = ('{:>3}  {:>6}  {:>5} {:>12}  {:>14}  {:>14} {:>14}  {:>18}  {:>22} {:>20} '.format(
-        weapon_list[0].get_name(), weapon_list[0].get_stat(), weapon_list[1].get_stat(),
-        weapon_list[2].get_stat(), weapon_list[3].get_stat(), weapon_list[4].get_stat(),
-        weapon_list[5].get_stat(), weapon_list[6].get_stat(), weapon_list[7].get_stat(), weapon_list[0].get_rof()))
+    #header = ('Weapon\t Damage\t DPS\t STK vs 1 armor\t TTK vs 1 armor\t STK vs 2 armor\t TTK vs 2 armor\t '
+    #      'STK vs 3 armor/1+rook\t TTK vs 3 armor/1+rook\t Rate of Fire\n')
 
-    return header, stats
+    #stats = ('{:>3}  {:>6}  {:>5} {:>12}  {:>14}  {:>14} {:>14}  {:>18}  {:>22} {:>20} '.format(
+    #    weapon_list[0].get_name(), weapon_list[0].get_stat(), weapon_list[1].get_stat(),
+    #    weapon_list[2].get_stat(), weapon_list[3].get_stat(), weapon_list[4].get_stat(),
+    #    weapon_list[5].get_stat(), weapon_list[6].get_stat(), weapon_list[7].get_stat(), weapon_list[0].get_rof()))
+
+    message_tuple = ('Weapon\t', weapon_list[0].get_name(), '\t\t\tDamage\t',
+                     weapon_list[0].get_stat(), '\t\tDPS\t', weapon_list[1].get_stat(),
+                     '\t\tRate of Fire\t', weapon_list[0].get_rof(), '\n',
+                     'STK vs 1 armor\t', weapon_list[2].get_stat(), '\t\tTTK vs 1 armor\t',
+                     weapon_list[3].get_stat(), '\t\tSTK vs 2 armor\t', weapon_list[4].get_stat(), '\n',
+                     'TTK vs 2 armor\t', weapon_list[5].get_stat(), '\tSTK vs 3 armor/1+rook\t',
+                     weapon_list[6].get_stat(), '\tTTK vs 3 armor/1+rook\t', weapon_list[7].get_stat())
+
+    message = tuple_to_string(message_tuple)
+
+    weapon_list.clear()
+    found = False
+    #return header, stats
+    return message
 
 #For debugging
 #def main():
-#    read_file("CSRX 300")
+#    read_file('CSRX 300')
 
 
-#if __name__ == "__main__":
+#if __name__ == '__main__':
 #    main()
 
 @client.event
@@ -82,18 +109,23 @@ async def on_message(message):
     if message.content.startswith('!weaponstat'):
         string_array = message.content.split()
         if len(string_array) == 1:
-            await message.channel.send("Please give a weapon name for the command to work!\n"
-                                       "Type !help to see more.")
+            await message.channel.send('Please give a weapon name for the command to work!\n'
+                                       'Type !help to see more.')
             return
+
+        if len(string_array) == 3:
+            string_array[1] = string_array[1] + ' ' + string_array[2]
+
         weapon_name = string_array[1]
-        header, stats = read_file(weapon_name)
-        await message.channel.send(header+stats)
+        message_to_send = read_file(weapon_name)
+        await message.channel.send(message_to_send)
+        return
 
     if message.content == '!help':
-        await message.channel.send("To use this bot type '!weaponstat gun_name' without the ' ' (single quotes).\n "
-              "E.g. !weaponstat F2 to get the stats for the F2.\n"
-              "Don't forget to type the actual name of the gun, smg-11 and not smg11.\n \n"
-              "Stats are taken from Rogue-9's spreadsheet.")
+        await message.channel.send('To use this bot type "!weaponstat gun_name" without the " " (double quotes).\n '
+              'E.g. !weaponstat F2 to get the stats for the F2.\n'
+              'Don\'t forget to type the actual name of the gun, smg-11 and not smg11.\n \n'
+              'Stats are taken from Rogue-9\'s spreadsheet.')
 
 
 client.run(token)
